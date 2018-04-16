96fbb2e5c8351ee83fab203bf6b47b9507e770d3
chrisdanicic <c.danicic@ischool.berkeley.edu>
2018-04-16 01:05:07 -0500

    more trial and error

--- bidirectional_lstm_reluHiSig_softmax_toxic_test.py
+++ bidirectional_lstm_reluHiSig_softmax_toxic_test.py
@@ -26,7 +26,7 @@ FILE=sys.argv[1]  # file path
 num_classes = 6
 batch_size = int(sys.argv[2]) # try with 32
 maxlen = int(sys.argv[3])   # longest comment in words split(' ') is 2300, vast majority under 300
-max_features = 5000 # cut texts after this number of words, since character max is 5000, word max also has this ceiling
+max_features = 10000 # cut texts after this number of words, since character max is 5000, word max also has this ceiling
 EPOCHS=int(sys.argv[4])
 
 # import data
@@ -58,14 +58,14 @@ print('tokenizing')
 #top 10000 words, all lowercase
 Tokenizer= keras.preprocessing.text.Tokenizer(num_words=10000, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True, split=' ', char_level=False, oov_token=None)
 
-Tokenizer.fit_on_texts(train_all)
+Tokenizer.fit_on_texts(train_all['comment_text'])
 x_train=Tokenizer.texts_to_sequences(x_train)
 x_test=Tokenizer.texts_to_sequences(x_test)
 
 
 print('Padding sequences')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen,truncating='post') +0.00001
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen,truncating='post') +0.00001
 
 #check shape
 print('x_train shape:', x_train.shape)
@@ -74,13 +74,14 @@ y_train = np.array(y_train)
 y_test = np.array(y_test)
 
 model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(Embedding(max_features, 128, input_length=maxlen,trainable=False))
 model.add(Bidirectional(LSTM(64,activation='relu',recurrent_activation='hard_sigmoid')))  #relu for activation, hard sigmoid bc fast and reliable
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-# try using different optimizers and different optimizer configs
-model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+# adam optimizer and gradient clipping to reduce explosion --> NaN loss
+adam_opt=keras.optimizers.Adam(amsgrad=True, clipnorm=1.0, clipvalue=0.5)
+model.compile(adam_opt, 'categorical_crossentropy', metrics=['accuracy'])
 
 print('Training...')
 model.fit(x_train, y_train,
@@ -88,4 +89,4 @@ model.fit(x_train, y_train,
           epochs=EPOCHS,
           validation_data=[x_test, y_test])
 
-model.save('bidirectional_lstm_reluHiSig_softmax_{}batch_{}epochs.h5'.format(batch_size,EPOCHS))
+model.save('bidirectional_lstm_reluHiSig_softmax_{}batch_{}maxlen_{}epochs.h5'.format(batch_size,maxlen,EPOCHS))
